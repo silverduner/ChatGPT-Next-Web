bb462fb6929a43c5eaacefe93cb88828a964385c
Yuwen Sun <silverduner@gmail.com>
2023-06-19 09:44:28 -0400

    remove unavailable models

--- app/store/app.ts
+++ app/store/app.ts
@@ -73,27 +73,11 @@ const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
-    name: "gpt-4",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: ENABLE_GPT4,
-  },
-  {
     name: "gpt-3.5-turbo",
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
+    name: "gpt-3.5-turbo-16k",
     available: true,
   },
 ];
